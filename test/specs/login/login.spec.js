23f0987e3aa8ca6a23019034a624de80cb468077
Rodela <naziahossain1celloscope@gmail.com>
2025-03-13 10:49:39 +0600

    Initial commit

--- test/specs/login/login.spec.js
+++ test/specs/login/login.spec.js
@@ -2,7 +2,7 @@ const { expect } = require('@wdio/globals');
 const LoginPage = require('./login.po');
 
 describe('Login Test', () => {
-    it('1. Successful Login Test', async () => {
+    it('1. Verify successful user authentication', async () => {
         //Enter phone number
         await LoginPage.openLoginScreen();
         
