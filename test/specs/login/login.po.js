10413a44c0039977664f579918523290302954cb
Rodela <naziahossain1celloscope@gmail.com>
2025-03-16 13:49:54 +0600

    Changes made in script

--- test/specs/login/login.po.js
+++ test/specs/login/login.po.js
@@ -5,12 +5,15 @@ class LoginPage {
         //const usernum = data.login.username
 
         await driver.pause(1000);
+        //click on get started
+        const getStarted = await $('~Get Started');
+        await getStarted.click();
         //enter mobile number
         const mobileno = await $('android.widget.EditText');
         await mobileno.click();
-        const usernum = data.login.username
-        await mobileno.addValue(usernum);
-        //await mobileno.addValue('01933449933');
+        //const usernum = data.login.username
+        //await mobileno.addValue(usernum);
+        mobileno.addValue('01933449933');
         await driver.pause(2000);
         //click on Next button
         const nextButton = await $('~Next');
