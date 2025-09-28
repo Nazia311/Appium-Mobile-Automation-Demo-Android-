7b82bc74df41d6cc02c476586a2e40a8f2bdc2e6
Rodela <naziahossain1celloscope@gmail.com>
2025-09-28 13:09:08 +0600

    New add of project

--- test/specs/appfeature/appfeature.po.js
+++ test/specs/appfeature/appfeature.po.js
@@ -18,7 +18,7 @@ class app {
           await getText.click();
           await driver.pause(2000);
         // check the text is hello
-        const clipboardText = $('~clipboardText');
+        const clipboardText = $('~Hello');
         const text = await clipboardText.getText();
         if(text==='Hello'){
             console.log('Clipboard text is correct');
@@ -31,22 +31,24 @@ class app {
   }
   async listDemo(){
     // click list demo
-    const listDemo = $('//android.view.ViewGroup[@resource-id="RNE__LISTITEM__padView"])[4]');
+    const listDemo = $('(//android.view.ViewGroup[@resource-id="RNE__LISTITEM__padView"])[6]');
     await listDemo.click();
     await driver.pause(2000);
-    // scroll down
-    await driver.touchAction([
-        { action: 'press', x: 500, y: 1500 },
-        { action: 'moveTo', x: 500, y: 500 },
-        'release'
-    ]);
-    await driver.pause(2000);
-    // scroll up
-    await driver.touchAction([
-        { action: 'press', x: 500, y: 500 },
-        { action: 'moveTo', x: 500, y: 1500 },
-        'release'
-    ]);
+
+      await driver.performActions([
+          {
+              type: 'pointer',
+              id: 'finger1',
+              parameters: { pointerType: 'touch' },
+              actions: [
+                  { type: 'pointerMove', duration: 0, x: 500, y: 1500 },
+                  { type: 'pointerDown', button: 0 },
+                  { type: 'pointerMove', duration: 1000, x: 500, y: 500 },
+                  { type: 'pointerUp', button: 0 }
+              ]
+          }
+      ]);
+
     await driver.pause(2000);
 
     // click AWS 
@@ -55,9 +57,10 @@ class app {
     await driver.pause(2000);
     // click ok
     const ok = $('//android.widget.Button[@resource-id="android:id/button1"]');
-    await ok.click();
-    await driver.pause(2000);
-
+    if (await ok.isDisplayed()) {
+      await ok.click();
+      console.log('Permission dialog handled.');
+    }
     // click back
     const back = $('~Navigate Up');
     await back.click();
@@ -71,16 +74,28 @@ class app {
     const picker = $('~dayPicker');
     await picker.click();
     await driver.pause(2000);
-    const day = $('//android.widget.CheckedTextView[@resource-id="android:id/text1" and @text="8"]]');
-    await day.click();
+    const day = $('//android.widget.CheckedTextView[@resource-id="android:id/text1" and @text="8"]');
+    if (await day.isDisplayed()) {
+      console.log('The date element is displayed.');
+      const dayText = await day.getText();
+      console.log(`The date being selected is: ${dayText}`);
+      await day.click();
     await driver.pause(2000);
+    }
     // click learn more
     const learnMore = $('~learnMore');
     await learnMore.click();
+    //click date 
     await driver.pause(2000);
-    //click ok
-    const oknow = $('//android.widget.Button[@resource-id="android:id/button1"]');
-    await oknow.click();
+    const okButton = $('//android.widget.Button[@resource-id="android:id/button1"]');
+    // Wait for the OK button to be displayed
+    if (await okButton.waitForDisplayed({ timeout: 5000 })) {
+        console.log('Dialog box appeared. Clicking OK.');
+        await okButton.click();
+        console.log('Permission dialog handled.');
+    } else {
+        console.log('OK button not found or dialog box did not appear.');
+    }
     // click back
     const back = $('~Navigate Up');
     await back.click();
@@ -106,7 +121,7 @@ class app {
       await getText.click();
       await driver.pause(2000);
     // check the text is hello
-    const clipboardText = $('~clipboardText');
+    const clipboardText = $('~Hello');
     const text = await clipboardText.getText();
     if(text==='Hello'){
         console.log('Clipboard text is correct first time');
